5f3373bd4e193c779a191f9ec628888031100a90
benja <benjavallory@hotmail.com>
2023-12-12 00:37:42 -0300

    asdadsada

--- src/App.vue
+++ src/App.vue
@@ -22,20 +22,37 @@ export default {
 }
 #app{
   width: 100%;
-  height: 550vh;
   background: rgb(7,15,48);
   background: linear-gradient(90deg, rgba(7,15,48,1) 0%, rgba(20,37,112,1) 50%, rgba(7,15,48,1) 100%);
+}
+@media only screen and (min-width: 320px) and (max-width: 639px) {
+  #app{
+  width: 100%;
+  height: 560vh;
+  }
+}
+@media only screen and (min-width: 640px) and (max-width: 767px) {
+  #app{
+  width: 100%;
+  height: 570vh;
+  }
 }
 @media only screen and (min-width: 768px) and (max-width: 1023px) {
   #app{
   width: 100%;
-  height: 340vh;
+  height: 300vh;
   }
 }
-@media only screen and (min-width: 1024px) and (max-width: 1279px){
+@media only screen and (min-width: 1024px) and (max-width: 1200px){
   #app{
   width: 100%;
   height: 370vh;
+  }
+}
+@media only screen and (min-width: 1201px) and (max-width: 1279px){
+  #app{
+  width: 100%;
+  height: 330vh;
   }
 }
 @media only screen and (min-width: 1280px) and (max-width: 1535px){
