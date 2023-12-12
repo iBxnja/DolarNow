3a68562d6e9083388565594c4ba8013e1e7c1a0b
benja <benjavallory@hotmail.com>
2023-12-12 13:09:31 -0300

    cambios

--- src/components/Conversor.vue
+++ src/components/Conversor.vue
@@ -4,26 +4,30 @@
     md:flex md:items-center md:justify-center md:flex-col md:w-full
     lg:flex lg:items-center lg:justify-center lg:flex-col lg:w-full
     xl:flex xl:items-center xl:justify-center xl:flex-col xl:w-full
+    2xl:flex 2xl:items-center 2xl:justify-center 2xl:flex-col 2xl:w-full
     ">
       <div class="cel:w-full cel:h-20 cel:grid cel:place-items-center
       sm:w-full sm:h-20 sm:grid sm:place-items-center
       md:w-full md:h-20 md:grid md:place-items-center
       lg:w-full lg:h-20 lg:grid lg:place-items-center
       xl:w-full xl:h-20 xl:grid xl:place-items-center
+      2xl:w-full 2xl:h-20 2xl:grid 2xl:place-items-center
       ">
-        <h1 class="text-white cel:text-3xl sm:text-3xl font-semibold md:text-4xl lg:text-5xl xl:text-5xl">CONVERSOR</h1>
+        <h1 class="text-white cel:text-3xl sm:text-3xl font-semibold md:text-4xl lg:text-5xl xl:text-5xl 2xl:text-5xl">CONVERSOR</h1>
       </div>
       <div class="cel:w-full cel:flex cel:flex-col cel:justify-center cel:items-center
       sm:w-full sm:flex sm:flex-col sm:justify-center sm:items-center
       md:w-full md:flex md:flex-col md:justify-center md:items-center
       lg:w-full lg:flex lg:flex-col lg:justify-center lg:items-center
       xl:w-full xl:flex xl:flex-col xl:justify-center xl:items-center
+      2xl:w-full 2xl:flex 2xl:flex-col 2xl:justify-center 2xl:items-center
       ">
         <label for="numeroCambioDolar" class="text-white cel:mt-5 cel:mb-2 cel:font-thin cel:text-sm
         sm:mt-5 sm:mb-2 sm:font-normal sm:text-base
         md:mt-5 md:mb-2 md:font-medium md:text-lg
         lg:mt-5 lg:mb-2 lg:font-medium lg:text-lg
         xl:mt-5 xl:mb-2 xl:font-medium xl:text-lg
+        2xl:mt-5 2xl:mb-2 2xl:font-medium 2xl:text-lg
         ">Dolares</label>
         <input type="number" name="numeroCambioDolar" id="numeroCambioDolar" inputmode="numeric"
           class="cel:w-3/4 cel:h-8 cel:border-none cel:mb-5 cel:text-center cel:text-black
@@ -31,6 +35,7 @@
           md:w-3/4 md:h-8 md:border-none md:mb-5 md:text-center md:text-black
           lg:w-3/4 lg:h-8 lg:border-none lg:mb-5 lg:text-center lg:text-black
           xl:w-3/4 xl:h-8 xl:border-none xl:mb-5 xl:text-center xl:text-black
+          2xl:w-3/4 2xl:h-8 2xl:border-none 2xl:mb-5 2xl:text-center 2xl:text-black
           ">
       </div>
       <div class="cel:w-full cel:flex cel:flex-col cel:justify-center cel:items-center
@@ -38,12 +43,14 @@
       md:w-full md:flex md:flex-col md:justify-center md:items-center
       lg:w-full lg:flex lg:flex-col lg:justify-center lg:items-center
       xl:w-full xl:flex xl:flex-col xl:justify-center xl:items-center
+      2xl:w-full 2xl:flex 2xl:flex-col 2xl:justify-center 2xl:items-center
       ">
         <label for="selectDolar" class="cel:text-white cel:mt-5 cel:mb-2 cel:font-thin cel:text-sm
         text-white sm:mt-5 sm:mb-2 sm:font-normal sm:text-base
           md:mt-5 md:mb-2 md:font-medium md:text-lg
           lg:mt-2 lg:mb-2 lg:font-medium lg:text-lg
           xl:mt-2 xl:mb-2 xl:font-medium xl:text-lg
+          2xl:mt-2 2xl:mb-2 2xl:font-medium 2xl:text-lg
           ">Seleccionar Dolar</label>
         <select v-model="tipoDolar" name="selectDolar" id="selectDolar"
           class="cel:w-3/4 cel:h-8 cel:border-none cel:mb-5 cel:text-center cel:text-black cel:overflow-y-auto
@@ -51,6 +58,7 @@
           md:w-3/4 md:h-8 md:border-none md:mb-5 md:text-center md:text-black md:overflow-y-auto
           lg:w-3/4 lg:h-8 lg:border-none lg:mb-5 lg:text-center lg:text-black lg:overflow-y-auto
           xl:w-3/4 xl:h-8 xl:border-none xl:mb-5 xl:text-center xl:text-black xl:overflow-y-auto
+          2xl:w-3/4 2xl:h-8 2xl:border-none 2xl:mb-5 2xl:text-center 2xl:text-black 2xl:overflow-y-auto
           ">
           <option class="cel:text-xs cel:max-h-16" v-for="tipo in dolares" :key="tipo.id" :value="tipo.venta">{{ tipo.casa
           }} - venta</option> 
@@ -61,20 +69,24 @@
       md:w-full md:flex md:flex-col md:justify-center md:items-center
       lg:w-full lg:flex lg:flex-col lg:justify-center lg:items-center
       xl:w-full xl:flex xl:flex-col xl:justify-center xl:items-center
+      2xl:w-full 2xl:flex 2xl:flex-col 2xl:justify-center 2xl:items-center
       ">
         <label for="resultado" class="cel:text-white cel:mt-5 cel:mb-2 cel:font-thin cel:text-sm
         text-white sm:mt-5 sm:mb-2 sm:font-normal sm:text-base
           md:mt-5 md:mb-2 md:font-medium md:text-lg
           lg:mt-5 lg:mb-2 lg:font-medium lg:text-lg
           xl:mt-5 xl:mb-2 xl:font-medium xl:text-lg
+          2xl:mt-5 2xl:mb-2 2xl:font-medium 2xl:text-lg
           ">Cambio a pesos
           argentinos</label>
         <input type="text" name="resultado" id="resultado" v-model="resultado" disabled
-          class="cel:w-3/4 cel:h-8 cel:border-none cel:mb-5 cel:text-center cel:text-white
+          class="cel:w-3/4 cel:h-8 cel:border-none cel:mb-5 cel:text-center cel:text-white text-center
           sm:w-3/4 sm:h-8 sm:border-none sm:mb-5 sm:text-center sm:text-white
           md:w-3/4 md:h-8 md:border-none md:mb-5 md:text-center md:text-white
           lg:w-3/4 lg:h-8 lg:border-none lg:mb-5 lg:text-center lg:text-white
           xl:w-3/4 xl:h-8 xl:border-none xl:mb-5 xl:text-center xl:text-white
+          2xl:w-3/4 2xl:h-8 2xl:border-none 2xl:mb-5 2xl:text-center 2xl:text-white
+          
           ">
       </div>
       <button type="submit" @click="capturarInputs" class="cel:w-40 cel:h-12 cel:bg-white cel:rounded-full cel:mt-5
@@ -82,6 +94,7 @@
       md:w-40 md:h-12 md:bg-white md:rounded-full md:mt-5
       lg:w-40 lg:h-12 lg:bg-white lg:rounded-full lg:mt-5
       xl:w-40 xl:h-12 xl:bg-white xl:rounded-full xl:mt-5
+      2xl:w-40 2xl:h-12 2xl:bg-white 2xl:rounded-full 2xl:mt-5
       mb-3
       ">CONVERTIR</button>
     </section>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,18 +4,21 @@
   md:flex md:items-center md:justify-center md:flex-col md:w-full
   lg:flex lg:items-center lg:justify-center lg:flex-col lg:w-full
   xl:flex xl:items-center xl:justify-center xl:flex-col xl:w-full
+  2xl:flex 2xl:items-center 2xl:justify-center 2xl:flex-col 2xl:w-full
  ">
       <div class="cel:w-full cel:h-20 cel:grid cel:place-items-center
       sm:w-full sm:h-20 sm:grid sm:place-items-center
       md:w-full md:h-20 md:grid md:place-items-center
       lg:w-full lg:h-20 lg:grid lg:place-items-center
       xl:w-full xl:h-20 xl:grid xl:place-items-center
+      2xl:w-full 2xl:h-20 2xl:grid 2xl:place-items-center
       ">
         <h1 class="cel:text-white cel:text-3xl cel:font-semibold
         sm:text-white sm:text-3xl sm:font-semibold
         md:text-white md:text-5xl md:font-semibold
         lg:text-white lg:text-5xl lg:font-semibold
         xl:text-white xl:text-5xl xl:font-semibold
+        2xl:text-white 2xl:text-5xl 2xl:font-semibold
         ">DOLAR AHORA</h1>
       </div>
       <div class="cel:w-full cel:flex cel:flex-col cel:justify-center cel:items-center
@@ -23,28 +26,31 @@
       md:w-full md:flex md:flex-wrap md:justify-center md:items-center
       lg:w-full lg:flex lg:flex-wrap lg:justify-center lg:items-center
       xl:w-full xl:flex xl:flex-wrap xl:justify-center xl:items-center
+      2xl:w-full 2xl:flex 2xl:flex-wrap 2xl:justify-center 2xl:items-center
       ">
         <div class="cel:w-3/4 cel:h-56 cel:border-black cel:shadow-md cel:shadow-black cel:border-2 cel:rounded-2xl cel:bg-gray-900 cel:mt-5 cel:mb-5 cel:flex cel:flex-col cel:justify-center cel:items-center
         sm:w-3/4 sm:h-56 sm:border-black sm:shadow-md sm:shadow-black sm:border-2 sm:rounded-2xl sm:bg-gray-900 sm:mt-5 sm:mb-5 sm:flex sm:flex-col sm:justify-center sm:items-center
         md:w-64 md:ml-10 md:mr-10 md:h-56 md:border-black md:shadow-md md:shadow-black md:border-2 md:rounded-2xl md:bg-gray-900 md:mt-5 md:mb-5 md:flex md:flex-col md:justify-center md:items-center
         lg:w-72 lg:ml-10 lg:mr-10 lg:h-64 lg:border-black lg:shadow-md lg:shadow-black lg:border-2 lg:rounded-2xl lg:bg-gray-900 lg:mt-5 lg:mb-5 lg:flex lg:flex-col lg:justify-center lg:items-center
         xl:w-72 xl:ml-10 xl:mr-10 xl:h-64 xl:border-black xl:shadow-md xl:shadow-black xl:border-2 xl:rounded-2xl xl:bg-gray-900 xl:mt-5 xl:mb-5 xl:flex xl:flex-col xl:justify-center xl:items-center
+        2xl:w-72 2xl:ml-10 2xl:mr-10 2xl:h-64 2xl:border-black 2xl:shadow-md 2xl:shadow-black 2xl:border-2 2xl:rounded-2xl 2xl:bg-gray-900 2xl:mt-5 2xl:mb-5 2xl:flex 2xl:flex-col 2xl:justify-center 2xl:items-center
         " v-for="tipos in dolares" :key="tipos.id">
             <h2 class= "uppercase text-white font-semibold sm:text-2xl md:text-2xl lg:text-2xl">{{ tipos.casa }}</h2>
             <div class="text-center cel:mt-2
             sm:text-center sm:mt-2 md:mt-3
             lg:text-center lg:mt-4
             xl:text-center xl:mt-4
+            2xl:text-center 2xl:mt-4
             ">
               <h3 class="text-red-600">Venta</h3>
-              <h3 class="text-red-600 font-semibold cel:text-2xl sm:text-2xl md:text-2xl lg:text-3xl xl:text-3xl">${{ tipos.venta }}</h3>
+              <h3 class="text-red-600 font-semibold cel:text-2xl sm:text-2xl md:text-2xl lg:text-3xl xl:text-3xl 2xl:text-3xl">${{ tipos.venta }}</h3>
             </div>
-            <div v-if="!tipos.compra == ''" class="text-center cel:mt-2 sm:mt-2 md:mt-3 lg:mt-4 xl:mt-4">
+            <div v-if="!tipos.compra == ''" class="text-center cel:mt-2 sm:mt-2 md:mt-3 lg:mt-4 xl:mt-4 2xl:mt-4">
               <h3 class="text-green-600">Compra</h3>
-              <h4 class="text-green-600 font-semibold cel:text-2xl sm:text-2xl md:text-2xl lg:text-3xl xl:text-3xl">${{ tipos.compra }}</h4>
+              <h4 class="text-green-600 font-semibold cel:text-2xl sm:text-2xl md:text-2xl lg:text-3xl xl:text-3xl 2xl:text-3xl">${{ tipos.compra }}</h4>
             </div>
-            <div class="text-center cel:mt-3 sm:mt-3">
-                <h6 v-if="dolares.length > 0" class="cel:text-xs text-white sm:text-base  md:text-xs md:mt-1 md:mb-1 mt-2 mb-2 lg:text-sm xl:text-sm">Ultima actualización: {{
+            <div class="text-center cel:mt-3 sm:mt-3 2xl:text-center">
+                <h6 v-if="dolares.length > 0" class="cel:text-xs text-white sm:text-base  md:text-xs md:mt-1 md:mb-1 mt-2 mb-2 lg:text-sm xl:text-sm 2xl:text-sm">Ultima actualización: {{
                     formatearFecha(dolares[0].fechaActualizacion) }}</h6>
             </div>
         </div>
